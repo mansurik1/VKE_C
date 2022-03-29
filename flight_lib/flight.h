8bb8af1252a47ae35740c9db0e90e99d22550b2c
Nizhametdinov Mansur <mansur.sh.n@yandex.ru>
2022-03-30 00:30:49 +0300

    linters_fix

--- flight_lib/flight.h
+++ flight_lib/flight.h
@@ -1,5 +1,5 @@
-#ifndef _HW1_FLIGHT_LIB_FLIGHT_H_
-#define _HW1_FLIGHT_LIB_FLIGHT_H_
+#ifndef FLIGHT_LIB_FLIGHT_H_
+#define FLIGHT_LIB_FLIGHT_H_
 
 #include <stdio.h>
 #include <errno.h>
@@ -45,4 +45,4 @@ int find_best_flights(char*, char*, Flight*, size_t, size_t*, size_t*);
 void print_flight(FILE*, Flight*, size_t);
 void free_flights_memory(Flight*, size_t);
 
-#endif  // _HW1_FLIGHT_LIB_FLIGHT_H_
+#endif  // FLIGHT_LIB_FLIGHT_H_
