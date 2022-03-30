3b7b7bdaaee22ad82554a5dad632f4bca13cabeb
Nizhametdinov Mansur <mansur.sh.n@yandex.ru>
2022-03-31 01:56:59 +0300

    Sanitizers fix

--- scripts/sanitizer_address.sh
+++ scripts/sanitizer_address.sh
@@ -23,6 +23,6 @@ function check_log() {
 
 print_header "RUN sanitizers"
 tree
-check_log "g++ -Wall -g -pthread -fsanitize=address,undefined,leak -fno-sanitize-recover=all -fsanitize-undefined-trap-on-error tests/test_flight.cpp -Iflight_lib -lgtest_main -lgtest -lpthread -lcat_lib -Lbuild/flight_lib -lgcov" "undefined reference"
+check_log "g++ -Wall -g -pthread -fsanitize=address,undefined,leak -fno-sanitize-recover=all -fsanitize-undefined-trap-on-error tests/test_flight.cpp -Iflight_lib -lgtest_main -lgtest -lpthread -flight_lib -Lbuild/flight_lib -lgcov" "undefined reference"
 
 print_header "SANITIZERS SUCCESS"
