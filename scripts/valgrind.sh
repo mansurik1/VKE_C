b8df6ec95b5bab175762e623263c8464c9097c9d
Nizhametdinov Mansur <mansur.sh.n@yandex.ru>
2022-03-30 21:07:04 +0300

    valgrind_fix

--- scripts/valgrind.sh
+++ scripts/valgrind.sh
@@ -22,6 +22,6 @@ function check_log() {
 }
 
 print_header "RUN valgrind"
-check_log "valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-leak-kinds=all build/tests/flight_test" "ERROR"
+check_log "valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-leak-kinds=all build/tests/flight_test" "LEAK SUMMARY"
 
 print_header "VALGRIND WORK COMPLETED SUCCESSFULLY!"
