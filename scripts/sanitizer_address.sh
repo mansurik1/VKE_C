3be79f340194b3af8ff74895806877d961d4cbbb
mansurik1 <66423279+mansurik1@users.noreply.github.com>
2022-03-31 12:32:48 +0300

    Update sanitizer_address.sh

--- scripts/sanitizer_address.sh
+++ scripts/sanitizer_address.sh
@@ -22,8 +22,6 @@ function check_log() {
 }
 
 print_header "RUN sanitizers"
-#check_log "g++ -Wall -g -pthread -fsanitize=address,undefined,leak -fno-sanitize-recover=all -fsanitize-undefined-trap-on-error -Iflight_lib -Lbuild/flight_lib -lpthread -lgtest -lgtest_main -lgcov -lflight_lib tests/test_flight.cpp" "undefined reference"
-#check_log "g++ -Wall -g -pthread -fsanitize=address,undefined,leak -fno-sanitize-recover=all -fsanitize-undefined-trap-on-error tests/test_toys_search.cpp -Itoys_catalogue_lib -lgtest_main -lgtest -lpthread -lcat_lib -Lcmake-build-debug/toys_catalogue_lib -lgcov" "undefined reference"
 check_log "g++ -Wall -g -pthread -fsanitize=address,undefined,leak -fno-sanitize-recover=all -fsanitize-undefined-trap-on-error tests/test_flight.cpp -Iflight_lib -lgtest_main -lgtest -lpthread -lflight_lib -Lbuild/flight_lib -lgcov" "undefined reference"
 
 print_header "SANITIZERS SUCCESS"
