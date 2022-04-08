6b376c5706571d209ad23e3733790b020157bcaf
Nizhametdinov Mansur <mansur.sh.n@yandex.ru>
2022-04-08 09:25:07 +0300

    Test fix

--- tests/test_flight.cc
+++ tests/test_flight.cc
@@ -114,17 +114,17 @@ TEST(Whole_program, two_flights) {
 
   FILE* input_file = fmemopen(input, sizeof(input), "r");
 
-  char* test_output = new char[1253];
-  FILE* output_file = fmemopen(test_output, sizeof(char) * 1253, "w");
+  char* test_output = new char[1252];
+  FILE* output_file = fmemopen(test_output, sizeof(char) * 1252, "w");
 
   execute_program(input_file, output_file);
 
-  putc('!', output_file);
+  putc('\0', output_file);
 
   fclose(input_file);
   fclose(output_file);
 
-  char* right_output = new char[1253];
+  char* right_output = new char[1252];
   memcpy(
       right_output,
       "Please enter the number of flights -> \nPlease enter the flight code of "
@@ -146,7 +146,7 @@ TEST(Whole_program, two_flights) {
       "$234.500000\n\nMost advantageous air ticket:\nFlight code: "
       "CAB14\nDeparture airport code: SVO\nArrival airport code: VKO\nFlight "
       "duration: 95 minutes\nFlight cost: $113.454002\n",
-      sizeof(char) * 1253);
+      sizeof(char) * 1252);
 
   EXPECT_EQ(strcmp(test_output, right_output), 0);
 
